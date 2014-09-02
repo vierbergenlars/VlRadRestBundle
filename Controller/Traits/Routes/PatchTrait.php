623476f5456b67c12b85093f11e09830084df31e
Lars Vierbergen <vierbergenlars@gmail.com>
2014-09-02 17:39:06 +0200

    Add PatchTrait route trait

---
+++ Controller/Traits/Routes/PatchTrait.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the RadRest package.
+ *
+ * (c) Lars Vierbergen <vierbergenlars@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes;
+
+use FOS\RestBundle\Controller\Annotations\View as AView;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\Form\Form;
+
+/**
+ * This trait provides routes for PATCH on a resource
+ */
+trait PatchTrait
+{
+    use AbstractBaseManipulateTrait;
+
+    /**
+     * @ApiDoc
+     * @AView
+     */
+    public function patchAction(Request $request, $id)
+    {
+        $ret = $this->getFrontendManager()->editResource($id, $request, true);
+
+        if($ret instanceof Form) {
+            $view = View::create($ret, Codes::HTTP_BAD_REQUEST)->setTemplateVar('form');
+        } else {
+            $view = $this->redirectTo('get', array('id'=>$ret->getId()))->setStatusCode(Codes::HTTP_NO_CONTENT);
+        }
+
+        return $this->handleView($view);
+    }
+}
